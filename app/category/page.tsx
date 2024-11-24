09cc2273ef7f31a535d936aa4d5fe55e330313ab
Sandis <sandissirmais8@gmail.com>
2024-11-24 14:04:45 +0200

    Vercel V7

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,4 +1,4 @@
-/* import { client } from "@/sanity/lib/client";
+import { client } from "@/sanity/lib/client";
 import React from 'react'
 import { Category } from "../utils/interface";
 
@@ -29,5 +29,3 @@ const page = async () => {
 }
 
 export default page
-
-*/
